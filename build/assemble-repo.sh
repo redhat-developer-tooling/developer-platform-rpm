712a793c9160b46051600ce4f70285f638ca1449
Denis Golovin <dgolovin@users.noreply.github.com>
2017-05-19 10:33:33 -0700

    Include .sha256 files into final zip along with rpm packages

--- build/assemble-repo.sh
+++ build/assemble-repo.sh
@@ -1,9 +1,13 @@
 mkdir -p rh-devsuite-repo
 cd rh-devsuite-repo
 curl http://${DOWNLOAD_HOST}/devel/candidates/jboss/devstudio/devsuite-1.4.0.TP/rpms/signed/rh-devsuite-1.4.0-1.el7.x86_64.rpm -O
+curl http://${DOWNLOAD_HOST}/devel/candidates/jboss/devstudio/devsuite-1.4.0.TP/rpms/signed/rh-devsuite-1.4.0-1.el7.x86_64.rpm.sha256 -O
 curl http://${DOWNLOAD_HOST}/devel/candidates/jboss/devstudio/devstudio-10.4.0.GA/rpms/signed/rh-eclipse46-devstudio-10.4-0.20170511.1856.el7.x86_64.rpm -O
+curl http://${DOWNLOAD_HOST}/devel/candidates/jboss/devstudio/devstudio-10.4.0.GA/rpms/signed/rh-eclipse46-devstudio-10.4-0.20170511.1856.el7.x86_64.rpm.sha256 -O
 curl http://${DOWNLOAD_HOST}/rel-eng/Atomic/cdk/rpms/cdk-3.0.0/signed/cdk-minishift-3.0.0-1.el7.x86_64.rpm -O
+curl http://${DOWNLOAD_HOST}/rel-eng/Atomic/cdk/rpms/cdk-3.0.0/signed/cdk-minishift-3.0.0-1.el7.x86_64.rpm.sha256 -O
 curl http://${DOWNLOAD_HOST}/rel-eng/Atomic/cdk/rpms/cdk-3.0.0/signed/docker-machine-kvm-0.0.7-1.el7.x86_64.rpm -O
+curl http://${DOWNLOAD_HOST}/rel-eng/Atomic/cdk/rpms/cdk-3.0.0/signed/docker-machine-kvm-0.0.7-1.el7.x86_64.rpm.sh256 -O
 createrepo .
 cd ..
 zip devsuite-1.4.0-TP-RHEL.zip INSTALL.md LICENSE
