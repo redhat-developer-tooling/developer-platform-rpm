069cb0e21e9e7aa1603bbc698783d6fe03daac49
Denis Golovin <dgolovin@users.noreply.github.com>
2017-05-19 12:46:04 -0700

    Update sha check for devstudio rpm

--- build/assemble-repo.sh
+++ build/assemble-repo.sh
@@ -4,7 +4,7 @@ curl http://${DOWNLOAD_HOST}/devel/candidates/jboss/devstudio/devsuite-1.4.0.TP/
 curl http://${DOWNLOAD_HOST}/devel/candidates/jboss/devstudio/devsuite-1.4.0.TP/rpms/signed/rh-devsuite-1.4.0-1.el7.x86_64.rpm.sha256 -O
 sha256sum --check rh-devsuite-1.4.0-1.el7.x86_64.rpm.sha256
 curl http://${DOWNLOAD_HOST}/devel/candidates/jboss/devstudio/devstudio-10.4.0.GA/rpms/signed/rh-eclipse46-devstudio-10.4-0.20170511.1856.el7.x86_64.rpm -O
-curl http://${DOWNLOAD_HOST}/devel/candidates/jboss/devstudio/devstudio-10.4.0.GA/rpms/signed/rh-eclipse46-devstudio-10.4-0.20170511.1856.el7.x86_64.rpm.sha256 -O
+curl http://${DOWNLOAD_HOST}/devel/candidates/jboss/devstudio/devstudio-10.4.0.GA/rpms/signed/SHASUM -O
 sha256sum --check rh-eclipse46-devstudio-10.4-0.20170511.1856.el7.x86_64.rpm.sha256
 curl http://${DOWNLOAD_HOST}/rel-eng/Atomic/cdk/rpms/cdk-3.0.0/signed/cdk-minishift-3.0.0-1.el7.x86_64.rpm -O
 curl http://${DOWNLOAD_HOST}/rel-eng/Atomic/cdk/rpms/cdk-3.0.0/signed/cdk-minishift-3.0.0-1.el7.x86_64.rpm.sha256 -O
