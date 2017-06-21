83fc5fdc2bd04d09daeb7c3548b3eae8976e1b05
Denis Golovin <dgolovin@users.noreply.github.com>
2017-06-21 00:49:54 -0700

    Update file name for CDK to 3.0.0-2 build

--- build/assemble-repo.sh
+++ build/assemble-repo.sh
@@ -9,8 +9,8 @@ echo "734c5b2be60adbfefe809ec61698dcc243ce8ae3f39d108b399fd50db810d766 rh-eclips
 sha256sum --check rh-eclipse46-devstudio-10.4-0.20170511.1856.el7.x86_64.rpm.sha256 || exit 1
 
 curl http://${DOWNLOAD_HOST}/rel-eng/Atomic/cdk/rpms/cdk-3.0.0-2/cdk-minishift-3.0.0-2.el7.x86_64.rpm -O
-echo "ccaa98daced48b31916015d5be859b99abb5c66ef8c7ec62ae2feab03f2c4bbc cdk-minishift-3.0.0-1.el7.x86_64.rpm" > cdk-minishift-3.0.0-1.el7.x86_64.rpm.sha256
-sha256sum --check cdk-minishift-3.0.0-1.el7.x86_64.rpm.sha256 || exit 1
+echo "ccaa98daced48b31916015d5be859b99abb5c66ef8c7ec62ae2feab03f2c4bbc cdk-minishift-3.0.0-2.el7.x86_64.rpm" > cdk-minishift-3.0.0-2.el7.x86_64.rpm.sha256
+sha256sum --check cdk-minishift-3.0.0-2.el7.x86_64.rpm.sha256 || exit 1
 
 curl http://${DOWNLOAD_HOST}/rel-eng/Atomic/cdk/rpms/cdk-3.0.0/signed/docker-machine-kvm-0.0.7-1.el7.x86_64.rpm -O
 echo "63b3b02e11d9e4ac9d8c2ecab24653a3bf41e867a7a7cd165e202c070a1d2d58 docker-machine-kvm-0.0.7-1.el7.x86_64.rpm" > docker-machine-kvm-0.0.7-1.el7.x86_64.rpm.sha256
