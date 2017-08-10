bd4c3d8e8e23ff5fc696c9fe76dd101fc3931b20
Denis Golovin <dgolovin@redhat.com>
2017-08-10 02:28:35 -0400

    Move rpmbuild to project root

--- build/build-rpm.sh
+++ build/build-rpm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm -rf $(pwd)/build/SOURCES
-mkdir -p $(pwd)/build/SOURCES/
-tar -cvzf $(pwd)/build/SOURCES/rh-devsuite-src-2.0.0-1.tar.gz -C ./SOURCES/ rh-devsuite-2.0.0/
-rpmbuild -bb SPECS/rh-devsuite.spec --define "_rpmdir $(pwd)/build"
+rm -rf $(pwd)/rpmbuild/
+mkdir -p $(pwd)/rpmbuild/SOURCES/
+tar -cvzf $(pwd)/rpmbuild/SOURCES/rh-devsuite-src-2.0.0-1.tar.gz -C ./SOURCES/ rh-devsuite-2.0.0/
+rpmbuild -ba SPECS/rh-devsuite.spec --define "_topdir $(pwd)/rpmbuild"
