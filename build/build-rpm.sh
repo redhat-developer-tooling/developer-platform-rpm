e27fffad4363c91f6a79e7c97ba4b4f6e980be5e
Denis Golovin <dgolovin@users.noreply.github.com>
2017-08-09 22:53:37 -0700

    Redirect rpm build to build folder

--- build/build-rpm.sh
+++ build/build-rpm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rm -f ~/rpmbuild/SOURCES/rh-devsuite-src-2.0.0-1.tar.gz
-tar -cvzf ~/rpmbuild/SOURCES/rh-devsuite-src-2.0.0-1.tar.gz -C ./SOURCES/ rh-devsuite-2.0.0/
-rpmbuild -bb SPECS/rh-devsuite.spec
+rm -f $(pwd)/build/SOURCES/rh-devsuite-src-2.0.0-1.tar.gz
+tar -cvzf $(pwd)/build/SOURCES/rh-devsuite-src-2.0.0-1.tar.gz -C ./SOURCES/ rh-devsuite-2.0.0/
+rpmbuild -bb SPECS/rh-devsuite.spec --define "_rpmdir $(pwd)/build"
