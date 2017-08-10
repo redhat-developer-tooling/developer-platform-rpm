e22794e6eb02bc6f5e2eca47b00857a0a44b1703
Denis Golovin <dgolovin@redhat.com>
2017-08-09 20:28:12 -0400

    Remove sudo and create links to oc in ~/.local/bin

--- SOURCES/rh-devsuite-2.0.0/setup-cdk.sh
+++ SOURCES/rh-devsuite-2.0.0/setup-cdk.sh
@@ -1,14 +1,16 @@
-minishift setup-cdk --force --default-vm-driver="kvm"
 home="/home/$USER/.minishift"
 if [ -z ${MINISHIFT_HOME+x} ]
 then
-  echo "MINISHIFT_HOME is not set";
+  echo "MINISHIFT_HOME is $home";
 else
   echo "MINISHIFT_HOME is set to '$MINISHIFT_HOME'"
   home="${MINISHIFT_HOME}"
 fi
-
+minishift setup-cdk --force --default-vm-driver="kvm"
+#search for OpenShift CLI
 file=$(find $home -name "oc" -type "f")
 
-sudo rm -f /usr/bin/oc
-sudo ln -s $file /usr/bin/oc
+#add soft link to directory in PATH
+rm -f ~/.local/bin/oc
+mkdir -p ~/.local/bin
+ln -s $file ~/.local/bin/oc
