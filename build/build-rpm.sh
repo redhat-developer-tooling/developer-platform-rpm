b3cf9a5f1debc46f0c2b553f025f57742d0dd536
Denis Golovin <dgolovin@users.noreply.github.com>
2017-08-09 23:00:21 -0700

    Create target forlder for tar

--- build/build-rpm.sh
+++ build/build-rpm.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-rm -f $(pwd)/build/SOURCES/rh-devsuite-src-2.0.0-1.tar.gz
+rm -rf $(pwd)/build/SOURCES
+mkdir -p $(pwd)/build/SOURCES/
 tar -cvzf $(pwd)/build/SOURCES/rh-devsuite-src-2.0.0-1.tar.gz -C ./SOURCES/ rh-devsuite-2.0.0/
 rpmbuild -bb SPECS/rh-devsuite.spec --define "_rpmdir $(pwd)/build"
