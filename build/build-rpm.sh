7405cc573d4caccc4eace0b0a8771878c51d4b22
Denis Golovin <dgolovin@gmail.com>
2017-10-30 20:53:42 -0700

    Change dir before sHA256SUMS generation

--- build/build-rpm.sh
+++ build/build-rpm.sh
@@ -3,5 +3,7 @@ rm -rf $(pwd)/rpmbuild/
 mkdir -p $(pwd)/rpmbuild/SOURCES/
 tar -cvzf $(pwd)/rpmbuild/SOURCES/rh-devsuite-src-2.1.0-1.tar.gz -C ./SOURCES/ rh-devsuite-2.1.0/
 rpmbuild -ba SPECS/rh-devsuite.spec --define "_topdir $(pwd)/rpmbuild"
-sha256sum $(pwd)/rpmbuild/RPMS/x86_64/*.rpm > $(pwd)/rpmbuild/RPMS/x86_64/SHA256SUMS
-sha256sum $(pwd)/rpmbuild/SRPMS/*.rpm > $(pwd)/rpmbuild/SRPMS/SHA256SUMS
+cd $(pwd)/rpmbuild/RPMS/x86_64/
+sha256sum *.rpm > SHA256SUMS
+cd $(pwd)/rpmbuild/SRPMS/
+sha256sum *.rpm > SHA256SUMS
