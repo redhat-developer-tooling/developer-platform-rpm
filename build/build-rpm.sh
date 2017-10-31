dc52b44929aff43f9cbab4d3d185f14cc803c03c
Denis Golovin <dgolovin@gmail.com>
2017-10-30 20:37:48 -0700

    Update version to 2.1.0

--- build/build-rpm.sh
+++ build/build-rpm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -rf $(pwd)/rpmbuild/
 mkdir -p $(pwd)/rpmbuild/SOURCES/
-tar -cvzf $(pwd)/rpmbuild/SOURCES/rh-devsuite-src-2.0.1-1.tar.gz -C ./SOURCES/ rh-devsuite-2.0.1/
+tar -cvzf $(pwd)/rpmbuild/SOURCES/rh-devsuite-src-2.1.0-1.tar.gz -C ./SOURCES/ rh-devsuite-2.1.0/
 rpmbuild -ba SPECS/rh-devsuite.spec --define "_topdir $(pwd)/rpmbuild"
