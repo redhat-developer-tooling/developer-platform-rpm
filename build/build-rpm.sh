ea20a7f2778a578c52cf37148cb2cd58f9b20d78
Denis Golovin <dgolovin@gmail.com>
2017-10-30 21:00:35 -0700

    Fix change dir commands

--- build/build-rpm.sh
+++ build/build-rpm.sh
@@ -3,7 +3,9 @@ rm -rf $(pwd)/rpmbuild/
 mkdir -p $(pwd)/rpmbuild/SOURCES/
 tar -cvzf $(pwd)/rpmbuild/SOURCES/rh-devsuite-src-2.1.0-1.tar.gz -C ./SOURCES/ rh-devsuite-2.1.0/
 rpmbuild -ba SPECS/rh-devsuite.spec --define "_topdir $(pwd)/rpmbuild"
-cd $(pwd)/rpmbuild/RPMS/x86_64/
+pushd $(pwd)/rpmbuild/RPMS/x86_64/
 sha256sum *.rpm > SHA256SUMS
-cd $(pwd)/rpmbuild/SRPMS/
+popd
+pushd $(pwd)/rpmbuild/SRPMS/
 sha256sum *.rpm > SHA256SUMS
+popd
