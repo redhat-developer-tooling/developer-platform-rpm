1710185ce97474c56830d51859b108f0eeaedd3f
Denis Golovin <dgolovin@gmail.com>
2017-10-30 20:51:18 -0700

    Change locations for SHA256SUMS files

--- build/build-rpm.sh
+++ build/build-rpm.sh
@@ -3,5 +3,5 @@ rm -rf $(pwd)/rpmbuild/
 mkdir -p $(pwd)/rpmbuild/SOURCES/
 tar -cvzf $(pwd)/rpmbuild/SOURCES/rh-devsuite-src-2.1.0-1.tar.gz -C ./SOURCES/ rh-devsuite-2.1.0/
 rpmbuild -ba SPECS/rh-devsuite.spec --define "_topdir $(pwd)/rpmbuild"
-sha256sum $(pwd)/rpmbuild/RPMS/x86_64/*.rpm > SHA256SUMS
-sha256sum $(pwd)/rpmbuild/SRPMS/*.rpm > SHA256SUMS
+sha256sum $(pwd)/rpmbuild/RPMS/x86_64/*.rpm > $(pwd)/rpmbuild/RPMS/x86_64/SHA256SUMS
+sha256sum $(pwd)/rpmbuild/SRPMS/*.rpm > $(pwd)/rpmbuild/SRPMS/SHA256SUMS
