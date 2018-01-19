b5269c40131275a121226664662f83a7397a2c60
Denis Golovin <dgolovin@gmail.com>
2018-01-19 12:40:29 -0800

    Add sed expression to add version to the folder inside archive

--- build/build-rpm.sh
+++ build/build-rpm.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 rm -rf $(pwd)/rpmbuild/
 mkdir -p $(pwd)/rpmbuild/SOURCES/
-tar -cvzf $(pwd)/rpmbuild/SOURCES/rh-devsuite-src.tar.gz -C ./SOURCES/ rh-devsuite/
+tar --xform=s/rh-devsuite/rh-devsuite-2.2.0/ -cvzf $(pwd)/rpmbuild/SOURCES/rh-devsuite-src.tar.gz -C ./SOURCES/ rh-devsuite/
 rpmbuild -ba SPECS/rh-devsuite.spec --define "_topdir $(pwd)/rpmbuild"
 pushd $(pwd)/rpmbuild/RPMS/x86_64/
-sha256sum rh-devsuite-2.1.0-1.el7.x86_64.rpm > rh-devsuite-2.1.0-1.el7.x86_64.rpm.sha256
-sha256sum rh-devsuite-debuginfo-2.1.0-1.el7.x86_64.rpm > rh-devsuite-debuginfo-2.1.0-1.el7.x86_64.rpm.sha256
+sha256sum rh-devsuite-2.2.0-1.el7.x86_64.rpm > rh-devsuite-2.2.0-1.el7.x86_64.rpm.sha256
+sha256sum rh-devsuite-debuginfo-2.2.0-1.el7.x86_64.rpm > rh-devsuite-debuginfo-2.2.0-1.el7.x86_64.rpm.sha256
 popd
 pushd $(pwd)/rpmbuild/SRPMS/
-sha256sum rh-devsuite-2.1.0-1.el7.src.rpm > rh-devsuite-2.1.0-1.el7.src.rpm.sha256
+sha256sum rh-devsuite-2.2.0-1.el7.src.rpm > rh-devsuite-2.2.0-1.el7.src.rpm.sha256
 popd
