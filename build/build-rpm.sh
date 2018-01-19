667944b9fef8decd5e53fecd0f61bc7d16576a9b
Denis Golovin <dgolovin@gmail.com>
2018-01-19 12:51:58 -0800

    Extract version from spec file

--- build/build-rpm.sh
+++ build/build-rpm.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 rm -rf $(pwd)/rpmbuild/
 mkdir -p $(pwd)/rpmbuild/SOURCES/
-tar --xform=s/rh-devsuite/rh-devsuite-2.2.0/ -cvzf $(pwd)/rpmbuild/SOURCES/rh-devsuite-src.tar.gz -C ./SOURCES/ rh-devsuite/
+version = grep -o 'Version: [1-9]\{1\}.[0-9]\{1\}.[0-9]\{1\}' SPECS/rh-devsuite.spec | grep -o '[1-9]\{1\}.[0-9]\{1\}.[0-9]\{1\}'
+tar --xform=s/rh-devsuite/rh-devsuite-$version/ -cvzf $(pwd)/rpmbuild/SOURCES/rh-devsuite-src.tar.gz -C ./SOURCES/ rh-devsuite/
 rpmbuild -ba SPECS/rh-devsuite.spec --define "_topdir $(pwd)/rpmbuild"
 pushd $(pwd)/rpmbuild/RPMS/x86_64/
 sha256sum rh-devsuite-2.2.0-1.el7.x86_64.rpm > rh-devsuite-2.2.0-1.el7.x86_64.rpm.sha256
