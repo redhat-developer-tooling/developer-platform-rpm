2c0ad24faf94b37fb19cc3b0b81c195842f22f9d
Denis Golovin <dgolovin@users.noreply.github.com>
2018-04-17 13:39:06 -0700

    Update versions in build scriprts

--- build/build-rpm.sh
+++ build/build-rpm.sh
@@ -6,9 +6,9 @@ echo Building Version: $version
 tar --xform=s/rh-devsuite/rh-devsuite-$version/ -cvzf $(pwd)/rpmbuild/SOURCES/rh-devsuite-src.tar.gz -C ./SOURCES/ rh-devsuite/
 rpmbuild -ba SPECS/rh-devsuite.spec --define "_topdir $(pwd)/rpmbuild"
 pushd $(pwd)/rpmbuild/RPMS/x86_64/
-sha256sum rh-devsuite-2.2.0-1.el7.x86_64.rpm > rh-devsuite-2.2.0-1.el7.x86_64.rpm.sha256
-sha256sum rh-devsuite-debuginfo-2.2.0-1.el7.x86_64.rpm > rh-devsuite-debuginfo-2.2.0-1.el7.x86_64.rpm.sha256
+sha256sum rh-devsuite-2.3.0-1.el7.x86_64.rpm > rh-devsuite-2.3.0-1.el7.x86_64.rpm.sha256
+sha256sum rh-devsuite-debuginfo-2.3.0-1.el7.x86_64.rpm > rh-devsuite-debuginfo-2.3.0-1.el7.x86_64.rpm.sha256
 popd
 pushd $(pwd)/rpmbuild/SRPMS/
-sha256sum rh-devsuite-2.2.0-1.el7.src.rpm > rh-devsuite-2.2.0-1.el7.src.rpm.sha256
+sha256sum rh-devsuite-2.3.0-1.el7.src.rpm > rh-devsuite-2.3.0-1.el7.src.rpm.sha256
 popd
